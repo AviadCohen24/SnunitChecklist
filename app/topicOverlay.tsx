c88004251162d838dc505c06d13fb7e4266f2906
Aviad Cohen Gvoora <avi27200p@gmail.com>
2024-01-06 02:57:22 +0200

    Created 'Test before ignition' operation json object

--- app/topicOverlay.tsx
+++ app/topicOverlay.tsx
@@ -1,24 +1,58 @@
 import { Text, StyleSheet, View } from 'react-native';
-import { useNavigation, useRouter, useLocalSearchParams } from "expo-router";
+import { useCallback, useState } from 'react';
+import { useNavigation, useRouter, useLocalSearchParams, useFocusEffect } from "expo-router";
+import * as Content from '../constants/Content.json'
+import { ChecklistItem } from '../constants/Types';
+
+// If the `ImportRequiredTopic` function is called frrom multiple places, use useMemo hook
+const ImportRequiredTopic = (topicName: string): ChecklistItem => {
+    const topic = Object.values(Content).filter(topic => topic.title === topicName)[0];
+    if (topic) {
+        return topic as ChecklistItem;
+    }
+    return undefined;
+}
+
+const ImportBackground = (path: string) => {
+    const image = require('../assets/images/pngTopView.png');
+    console.log(image);
+    return image
+}
 
 export default function topicOverlay() {
     const navigation = useNavigation();
     const router = useRouter();
     const params = useLocalSearchParams();
     const { topicName } = params;
+    const [content, setContent] = useState<ChecklistItem>(null);
+
+    useFocusEffect(
+        useCallback(() => {
+            const loadedContent = ImportRequiredTopic(topicName as string);
+            setContent(loadedContent);
+        }, [topicName])
+    );
+    
+    if (!content) {
+        return (
+            <View style={styles().container}>
+                <Text>Loading...</Text>
+            </View>
+        );
+    }
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>{`Topic: ${topicName}`}</Text>
+        <View style={styles().container}>
+            <Text style={styles().topicTitle}>{topicName}</Text>
         </View>
     )
 }
 
-const styles = StyleSheet.create({
+const styles = (backgroundImgUrl = "") => StyleSheet.create({
     container: {
-        flex: 1
+        backgroundImage: ImportBackground(backgroundImgUrl)
     },
-    text: {
+    topicTitle: {
       fontSize: 25,
       lineHeight: 25,
       fontWeight: 'bold',
---
+++ constants/Types.ts
@@ -0,0 +1,24 @@
+// --------------Topic full export export type--------------
+export type StepDetail = {
+  stepName: string;
+  details: string;
+  subSteps?: (StepDetail | { subStepName: string; details: string; })[];
+};
+
+export type Operation = {
+  operationName?: string;
+  steps: StepDetail[];
+};
+
+export type Subtopic = {
+  name: string;
+  operations: Operation[];
+};
+
+export type ChecklistItem = {
+  title: string;
+  backgroundUrl: string;
+  subtopics: Subtopic[];
+};
+
+export type Checklist = ChecklistItem[];
