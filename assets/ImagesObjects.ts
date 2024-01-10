4fb5714166e468dbb66b561fa1f667f763d799f6
Aviad Cohen Gvoora <avi27200p@gmail.com>
2024-01-10 23:16:59 +0200

    Added all topics backgrounds

--- assets/ImagesObjects.ts
+++ assets/ImagesObjects.ts
@@ -2,5 +2,10 @@ export const SvgGroupOfPlanes = require('./images/svgGroupPlanes.svg');
 export const PngGroupOfPlanes = require('./images/pngGroupPlanes.png');
 export const PngTopView = require('./images/pngTopView.png');
 export const TopicBackgrounds = {
-    beforeIgnition: require('./images/pngFrontToBackView.png')
-}
+    inAir: require('./images/pngFrontToBackView.png'),
+    touchNGo: require('./images/pngLowFlightOnRiver.png'),
+    afterIgnition: require('./images/pngAfterIgnition.png'),
+    ignition: require('./images/pngIgnition.png'),
+    beforeIgnition: require('./images/beforeIgnition.png'),
+    land: require('./images/pngLand.png'),
+};
